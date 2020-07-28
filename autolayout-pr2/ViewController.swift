5c8017d5d9e16fba91a3c5435ffd611ac4c787c3
chiaqingwu <secret8419@gmail.com>
2020-07-28 20:36:23 +0800

    Initial Commit

---
+++ autolayout-pr2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout-pr2
+//
+//  Created by chiaqingwu on 2020/7/28.
+//  Copyright © 2020 Andrewwuuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
